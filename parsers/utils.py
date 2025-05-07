652550b9176c1bf67920eaee9599234cd0e7a2f5
neyxezz <bassboosthelp@gmail.com>
2025-05-07 15:30:45 +0800

    теперь запрос с пробелом в нике будет отправляться корректно

--- parsers/utils.py
+++ parsers/utils.py
@@ -1,5 +1,5 @@
 import requests, json
-
+import urllib.parse
 def _fetch_master_data() -> dict:
     try:
         response = requests.get("https://master1.ddnet.org/ddnet/15/servers.json")
@@ -10,9 +10,9 @@ def _fetch_master_data() -> dict:
 
 def _fetch_player_data(name) -> dict:
     try:
-        response = requests.get(f"https://ddstats.tw/player/json?player={name}")
+        params = {"player": name}
+        response = requests.get("https://ddstats.tw/player/json", params={"player": name}) #корректный запрос при пробеле в нике, но всеравно какие то проблемы...
         response.raise_for_status()
         return json.loads(response.text)
     except requests.exceptions.RequestException as e:
         raise Exception(f"Error parsing: {e}")
-
