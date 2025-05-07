dca485315c8942eb4b0b0fc52b48fe047351b119
neyxezz <bassboosthelp@gmail.com>
2025-05-07 15:29:36 +0800

    исправление функции обновления данных; некоторые функции перебирали данные и создавали свой список, теперь возвращаются данные, которые даны в мастере

--- parsers/playerstats.py
+++ parsers/playerstats.py
@@ -5,7 +5,7 @@ class PlayerStatsParser:
         self.response = response
 
     def update(self):
-        self.response = _fetch_player_stats_data()
+        self.response = _fetch_player_data()
 
     def _get_activity(self):
         try:
@@ -14,67 +14,61 @@ class PlayerStatsParser:
             return None
 
     def _get_playtime_per_month(self):
-        activities_list = {}
         try:
-            for activity in self.response["playtime_per_month"]:
-                activities_list[activity["year_month"]] = activity["seconds_played"]
-            return activities_list
+            return self.response["playtime_per_month"]
         except:
             return None
 
     def _get_most_played_locations(self):
-        locations_list = {}
         try:
-            for location in self.response["most_played_locations"]:
-                locations_list[location["key"]] = location["seconds_played"]
-            return locations_list
+            return self.response["most_played_locations"]
         except:
-            None
+            return None
 
     def _get_most_played_categories(self):
-        categories_list = {}
         try:
-            for category in self.response["most_played_categories"]:
-                categories_list[category["key"]] = category["seconds_played"]
-            return categories_list
+            return self.response["most_played_categories"]
         except:
             return None
 
     def _get_most_played_gametypes(self):
-        gametypes_list = {}
         try:
-            for gametype in self.response["most_played_gametypes"]:
-                gametypes_list[gametype["key"]] = gametype["seconds_played"]
-            return gametypes_list
+            return self.response["most_played_gametypes"]
         except:
-            None
+            return None
 
     def _get_favourite_teammates(self):
-        favourites_list = {}
         try:
-            for favourite in self.response["favourite_teammates"]:
-                favourites_list[favourite["name"]] = favourite["ranks_together"]
-            return favourites_list if favourites_list else None
+            return self.response["favourite_teammates"]
         except:
-            None
+            return None
 
     def _get_recent_finishes(self):
-        return self.response["recent_finishes"]
+        try:
+            return self.response["recent_finishes"]
+        except:
+            return None
 
     def get_raw_data(self):
         return self.response
 
     def get_total_seconds_played(self):
         general_activity = self._get_activity()
-        return general_activity["total_seconds_played"]
+        if general_activity:
+            return general_activity["total_seconds_played"]
+        return None
 
     def get_start_of_playtime(self):
         general_activity = self._get_activity()
-        return general_activity["start_of_playtime"]
+        if general_activity:
+            return general_activity["start_of_playtime"]
+        return None
 
     def get_average_seconds_played(self):
         general_activity = self._get_activity()
-        return general_activity["average_seconds_played"]
+        if general_activity:
+            return general_activity["average_seconds_played"]
+        return None
 
     def get_playtime(self):
         return self._get_playtime_per_month()
