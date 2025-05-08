7348275b394eed7fe637a28c194e81016250dfd5
neyxezz <bassboosthelp@gmail.com>
2025-05-08 22:56:11 +0800

    добавление функции получения данных карты

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,4 +1,5 @@
 from .servers import ServersParser
 from .clients import ClientsParser
 from .playerstats import PlayerStatsParser
-from .utils import _fetch_master_data, _fetch_player_data
+from .maps import MapsParser
+from .utils import _fetch_master_data, _fetch_player_data, _fetch_map_data
