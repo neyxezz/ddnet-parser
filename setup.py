0cf6fc4f71c9d214a89057604334e2b39f8243e2
neyxezz <bassboosthelp@gmail.com>
2025-07-06 19:02:15 +0800

    1.2.0->1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="ddnet_parser",
-    version="1.2.0",
+    version="1.3.0",
     author="neyxezz",
     author_email="bassboosthelp@gmail.com",
     description="Simple data parser from ddnet sites",
